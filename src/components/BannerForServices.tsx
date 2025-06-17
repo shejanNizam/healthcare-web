3035904f9e1bd37a655598fd9df499b494cf909c
nizam <nizam.shejan11@gmail.com>
2025-06-17 20:50:11 +0600

    some change in banner

--- src/components/BannerForServices.tsx
+++ src/components/BannerForServices.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function BannerForServices() {
   return (
     <div className="text-center my-12">
-      <h3>BannerForServices</h3>
+      <h3>BannerForServices here </h3>
       <div className="text-center mt-8">
         <Link href="/client-services">
           <button className="bg-[#E7F1F8] text-primary font-semibold cursor-pointer rounded-t-lg px-6 py-1 border-b-2 border-b-primary hover:bg-primary hover:text-white transition duration-300 ease-in-out">
