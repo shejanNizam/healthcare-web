d230fffef9d5c90b9aa1f6f860d443b0f5523ae2
nizam <nizam.shejan11@gmail.com>
2025-06-18 14:48:11 +0600

    error page modified

--- src/app/saved-jobs/page.tsx
+++ src/app/saved-jobs/page.tsx
@@ -1,46 +1,3 @@
-// "use client";
-
-// import CustomErrorPage from "@/components/CustomErrorPage";
-// import JobCard from "@/components/JobCard";
-// import { useSavedJobs } from "@/context/SavedJobsContext";
-// import { useGetJobsQuery } from "@/redux/features/jobs/jobsApi";
-
-// // // Example job data, replace with your actual data or API fetch
-// // const jobsData: Job[] = [
-// //   // your job objects here with at least _id, hospitalName, etc.
-// // ];
-
-// export default function SavedJobs() {
-//   const { savedJobIds } = useSavedJobs();
-
-//   const { data } = useGetJobsQuery(savedJobIds);
-
-//   const jobsData = data?.data?.allJobs;
-
-//   const savedJobs = jobsData.filter((job) => savedJobIds.includes(job._id));
-
-//   return (
-//     <div className="p-6">
-//       <h2 className="text-xl font-bold text-primary mb-6">
-//         Saved jobs: <span className="text-2xl">{savedJobs.length}</span>
-//       </h2>
-
-//       {savedJobs.length === 0 ? (
-//         <div className="bg-white rounded-lg shadow-sm p-8 text-center">
-//           <CustomErrorPage />
-//           <p>No saved jobs found.</p>
-//         </div>
-//       ) : (
-//         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-//           {savedJobs.map((job) => (
-//             <JobCard key={job._id} job={job} />
-//           ))}
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
 "use client";
 
 import CustomErrorPage from "@/components/CustomErrorPage";
@@ -61,7 +18,6 @@ export default function SavedJobs() {
             <div className="flex justify-center items-center h-80 w-80">
               <CustomErrorPage />
             </div>
-            <p>No saved jobs found.</p>
           </div>
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
--- src/components/CustomErrorPage.tsx
+++ src/components/CustomErrorPage.tsx
@@ -1,14 +1,7 @@
-import Image from "next/image";
-import error_image from "../assets/error/error_image.png";
-
 export default function CustomErrorPage() {
   return (
     <div>
-      <Image
-        className="w-full h-auto mx-auto p-4 "
-        src={error_image}
-        alt="error_image"
-      />
+      <p className="text-red-600 font-semibold"> Nothing found here! </p>
     </div>
   );
 }
