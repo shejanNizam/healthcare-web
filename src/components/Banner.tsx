5000f07f588e6fc508698887c8938ef815b137dd
nizam <nizam.shejan11@gmail.com>
2025-06-20 12:20:45 +0600

    modified the home page banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,10 +4,10 @@ import SearchBar from "./SearchBar";
 
 export default function Banner() {
   return (
-    <div className="bg-gradient-to-r from-primary to-primary/10 h-auto">
+    <div className="bg-gradient-to-r from-primary to-primary/40 md:to-primary/20 h-auto">
       <div className="bg-primary-light  flex flex-col md:flex-row items-center justify-around px-4 md:px-24 xl:px-40 w-full">
         <div className="w-full md:w-[50%] flex flex-col gap-4 md:gap-8 my-4">
-          <h1 className="text-3xl md:text-4xl xl:text-5xl text-white font-semibold">
+          <h1 className="text-3xl md:text-4xl xl:text-5xl text-white font-extrabold">
             Find a job that aligns with your interests and skills
           </h1>
           <p className="w-full md:w-[90%] text-white text-sm ">
