1e2b785336d91ad11aba46d72dd0752fca7d7476
nizam <nizam.shejan11@gmail.com>
2025-06-23 09:31:37 +0600

    production api added

--- src/app/saved-jobs/page.tsx
+++ src/app/saved-jobs/page.tsx
@@ -9,10 +9,10 @@ export default function SavedJobs() {
 
   return (
     <div className="py-10 container mx-auto">
-      <h2 className="text-xl font-bold text-primary mb-6">
+      <h2 className="text-xl font-bold text-primary mb-6 px-2">
         Saved jobs: <span className="text-2xl">{savedJobs.length}</span>
       </h2>
-      <div className="">
+      <div className="px-4 md:px-6 lg:px-8">
         {savedJobs.length === 0 ? (
           <div className="bg-white rounded-lg  p-8 w-full flex flex-col items-center justify-center">
             <div className="flex justify-center items-center h-80 w-80">
