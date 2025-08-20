eaa28c8244e0c4e76fe2a458a8cb055119aa7ab7
nizam <nizam.shejan11@gmail.com>
2025-08-20 19:39:15 +0600

    stuff solutions two route create

---
+++ src/app/staffing-solutions/page.tsx
@@ -0,0 +1,7 @@
+export default function StaffingSolutions() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h3>StaffingSolutions</h3>
+    </div>
+  );
+}
---
+++ src/app/workforce-management/page.tsx
@@ -0,0 +1,7 @@
+export default function WorkforceManagement() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h3>WorkforceManagement</h3>
+    </div>
+  );
+}
